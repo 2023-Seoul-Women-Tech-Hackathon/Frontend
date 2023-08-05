a866ef413368ffd19f539ff03077be0f5580da63
cottonpup <younggyoung.lee1@gmail.com>
2023-08-05 18:37:00 +0900

    Add kakaomap sdk dependency

---
+++ src/components/Map.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+
+const { kakao } = window;
+
+export const Map = () => {
+    useEffect(() => {
+        const container = document.getElementById('map');
+        const options = {
+            center: new kakao.maps.LatLng(33.450701, 126.570667),
+            level: 3,
+        };
+        new kakao.maps.Map(container, options);
+    }, []);
+
+    return <div id="map" className="h-screen w-full"></div>;
+};
